07d4f41d3d468ff73b80d279922d231d17bfd399
Sam Gleske <sag47@drexel.edu>
2013-09-10 23:46:31 -0400

    git-mirrors.sh now updates all mirrors.
    Now git-mirrors.sh can be added to a cron job
    to update all mirrors listed in gitlab.

--- git-mirrors.sh
+++ git-mirrors.sh
@@ -2,7 +2,11 @@
 #Tue Sep 10 23:01:08 EDT 2013
 
 #Include all user options
-
 . "$(dirname $0)/config.sh"
-echo "repo $repo_dir"
-echo "GitLab Group $gitlab_group"
+cd $(dirname $0)
+
+ls -1 "${repo_dir}/${gitlab_namespace}" | while read mirror;do
+  if ! ./update_mirror.sh "${mirror}" &> /dev/null;then
+    echo "Error: ./update_mirror.sh ${mirror}" 1>&2
+  fi
+done
