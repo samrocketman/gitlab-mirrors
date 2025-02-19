13be7f0850b1ad4dad28b0931661727ddc491d29
Bastian Wanner <bastian.wanner@inovex.de>
2025-02-19 09:43:25 +0100

    Write directly to stdout/stderr instead of file
    Background: Simplifies docker integration since we may not have write
    permissions to included path

--- git-mirrors.sh
+++ git-mirrors.sh
@@ -16,15 +16,11 @@ STATUS=0
 
 for mirror in $(find "${repo_dir}/${gitlab_namespace}" -name refs -type d ); do
   mirror=$(realpath -s --relative-to="${repo_dir}/${gitlab_namespace}" $mirror/..)
-  echo "$(date +'%Y-%m-%d %H:%M:%S') CRON Startup for ${mirror}" >> ${git_mirrors_dir}/cron.log
-  if ! ./update_mirror.sh "${mirror}" >> ${git_mirrors_dir}/cron.log 2>&1 ;then
-    red_echo "Error: ./update_mirror.sh ${mirror} (more information in ${git_mirrors_dir}/cron.log)" 1>&2
+  echo "$(date +'%Y-%m-%d %H:%M:%S') CRON Startup for ${mirror}"
+  if ! ./update_mirror.sh "${mirror}";then
+    red_echo "Error: ./update_mirror.sh ${mirror}" 1>&2
     STATUS=1
   fi
 done
-
-if [[ -n $DEBUG ]];then 
-  cat "${git_mirrors_dir}"/cron.log
-fi
 
 exit ${STATUS}
