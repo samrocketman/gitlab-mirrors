1358ebf6a595ba40c8d323171330a19c17bed8f2
Bastian Wanner <bastian.wanner@inovex.de>
2025-02-21 13:52:10 +0100

    Avoid unbound variables

--- git-mirrors.sh
+++ git-mirrors.sh
@@ -4,7 +4,7 @@
 #Created Tue Sep 10 23:01:08 EDT 2013
 
 set -u
-[[ -n $DEBUG ]] && set -x
+[[ -n ${DEBUG+x} ]] && set -x
 
 #Include all user options and dependencies
 git_mirrors_dir="${0%/*}"
@@ -15,7 +15,7 @@ cd "${git_mirrors_dir}" || exit
 STATUS=0
 
 for mirror in $(find "${repo_dir}/${gitlab_namespace}" -name refs -type d ); do
-  mirror=$(realpath -s --relative-to="${repo_dir}/${gitlab_namespace}" $mirror/..)
+  mirror=$(realpath -s --relative-to="${repo_dir}/${gitlab_namespace}" "$mirror"/..)
   echo "$(date +'%Y-%m-%d %H:%M:%S') CRON Startup for ${mirror}"
   if ! ./update_mirror.sh "${mirror}";then
     red_echo "Error: ./update_mirror.sh ${mirror}" 1>&2
